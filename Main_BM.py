5506005a009c0634fc13fc9566ace890429a1466
Bryan0122 <39491853+Bryan0122@users.noreply.github.com>
2018-07-13 20:42:46 -0500

    Version V.4
    Se cambiaron valores de entrada, se añadio funcion de exportar la matriz de colores y la matriz J

--- Main_BM.py
+++ Main_BM.py
@@ -5,10 +5,11 @@ import os
 import pyedflib
 import scipy.io as sio
 import numpy as np
-from pyqtgraph.Qt import QtCore, QtGui
+from pyqtgraph.Qt import QtCore,QtGui
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
 import matplotlib.pyplot as plt
+from json_encoder import json
 import time
 import argparse
 
@@ -97,66 +98,58 @@ class Main_L(object):
         l.toc(False)
 
     def load_BM_data(self,Lead_field,verts,faces,channels):
-    	#Cargar Lead Field
-    	L = sio.loadmat(Lead_field)
-    	L_8ch=L['L_8ch']
-    	#Cargar verts
-    	verts = sio.loadmat(verts)
-    	verts=verts['verts']
-    	#Cargar faces
-    	faces = sio.loadmat(faces)
-    	faces=faces['faces']
-    	faces=faces-1
-    	#Cargar Channels
-    	channels = sio.loadmat(channels)
-    	channels=channels['channels']
-    	channels = [str(''.join(letter)) for letter_array in channels[0] for letter in letter_array]
-    	return L_8ch,verts,faces,channels
-
-    def syntethic_EEG(self,L):
-        #Genera un EEG aleatorio
-    	[Nc,Nd]=np.shape(L)
-    	Y=np.random.rand(Nc,1)
-    	Y = Y-np.amin(Y)
-    	Y = Y / np.max(Y)
-    	return Y
+        #Cargar Lead Field
+        L = sio.loadmat(Lead_field)
+        L_8ch=L['L_8ch']
+        #Cargar verts
+        verts = sio.loadmat(verts)
+        verts=verts['verts']
+        #Cargar faces
+        faces = sio.loadmat(faces)
+        faces=faces['faces']
+        faces=faces-1
+        #Cargar Channels
+        channels = sio.loadmat(channels)
+        channels=channels['channels']
+        channels = [str(''.join(letter)) for letter_array in channels[0] for letter in letter_array]
+        return L_8ch,verts,faces,channels
 
     def source_reconstruction(self,Y,L,alfa,meth): 
 
         #Funcion que reconstruye en cualquiera de las dos tecnicas especificadas, ya sea 'LOR':Loreta , 'sLOR': sLoreta
         l=Main_L()
-    	[Nc,Nd]=np.shape(L)
-    	[Nc,Nt]=np.shape(Y)
-    	I=np.eye(Nc)
-    	V_ones=np.ones((Nc,1))
-    	V_ones_t=np.ones((1,Nc))
-    	H=I-(np.dot(V_ones,V_ones_t)/np.dot(V_ones_t,V_ones))
-    	L_c=np.dot(H,L)
-    	Y_c=np.dot(H,Y)
-    	B = np.linalg.pinv(np.dot(L_c,np.transpose(L_c))+alfa*H)
-    	T=np.dot(np.transpose(L_c),B)
-    	J_e=np.dot(T,Y_c)
-    	S_J=np.dot(T,L_c)
-    	if (meth=='sLOR'):
-    		J=l.sLORETA(S_J,J_e,Nd,Nt)
-    	if (meth=='LOR'):
-    		J=l.LORETA(S_J,J_e)
-    	return J
+        [Nc,Nd]=np.shape(L)
+        [Nc,Nt]=np.shape(Y)
+        I=np.eye(Nc)
+        V_ones=np.ones((Nc,1))
+        V_ones_t=np.ones((1,Nc))
+        H=I-(np.dot(V_ones,V_ones_t)/np.dot(V_ones_t,V_ones))
+        L_c=np.dot(H,L)
+        Y_c=np.dot(H,Y)
+        B = np.linalg.pinv(np.dot(L_c,np.transpose(L_c))+alfa*H)
+        T=np.dot(np.transpose(L_c),B)
+        J_e=np.dot(T,Y_c)
+        S_J=np.dot(T,L_c)
+        if (meth=='sLOR'):
+            J=l.sLORETA(S_J,J_e,Nd,Nt)
+        if (meth=='LOR'):
+            J=l.LORETA(S_J,J_e)
+        return J
 
     def LORETA(self,S_J,J_e):
         #Tecnica de Loreta
-    	J_Lor=np.dot(S_J,J_e)
-    	return J_Lor
+        J_Lor=np.dot(S_J,J_e)
+        return J_Lor
 
     def sLORETA(self,S_J,J_e,Nd,Nt):
         # Tecnica de sLoreta
-    	S_J_diag=np.diag(S_J)
-    	S_J_d_i=np.ones((1,Nd))/S_J_diag
-    	S_J_d_i=np.transpose(S_J_d_i)
-    	V_ones=np.ones((1,Nt))
-    	S_J_i=S_J_d_i*V_ones
-    	J_sLor=S_J_i*J_e**2
-    	return J_sLor
+        S_J_diag=np.diag(S_J)
+        S_J_d_i=np.ones((1,Nd))/S_J_diag
+        S_J_d_i=np.transpose(S_J_d_i)
+        V_ones=np.ones((1,Nt))
+        S_J_i=S_J_d_i*V_ones
+        J_sLor=S_J_i*J_e**2
+        return J_sLor
 
     def normalize_vector(self,J_energy):
         #Normalizar componentes del vector
@@ -165,18 +158,21 @@ class Main_L(object):
 
     def source_energy(self,J):
         #Concentrar la energia de las posibles fuentes
-    	[Nd,Nt]=np.shape(J)
-    	if (Nt==1):
-    		J_energy=J**2
-    	else:
-    		J_energy=J**2
-    		J_energy=np.sum(J,1)
-    	return J_energy
+        [Nd,Nt]=np.shape(J)
+        if (Nt==1):
+            J_energy=J**2
+        else:
+            J_energy=J**2
+            J_energy=np.sum(J,1)
+        return J_energy
 
     def IntensitytoJet(self,Zrgb):
         l=Main_L()
-        cmap = np.array([[0.5,0.5,.5],[0,	0,	0.625],[0,	0,	0.6875],[0,	0,	0.75],[0,	0,	0.8125],[0,	0,	0.875],[0,	0,	0.9375],[0,	0,	1],[0,	0.0625,	1],[0,	0.125,	1],[0,	0.1875,	1],[0,	0.25,	1],[0,	0.3125,	1],[0,	0.375,	1],[0,	0.4375,	1],[0,	0.5,	1],[0,	0.5625,	1],[0,	0.625,	1],[0,	0.6875,	1],[0,	0.75,	1],[0,	0.8125,	1],[0,	0.875,	1],[0,	0.9375,	1],[0,	1,	1],[0.0625,	1,	0.9375],[0.125,	1,	0.875],[0.1875,	1,	0.8125],[0.25,	1,	0.75],[0.3125,	1,	0.6875],[0.375,	1,	0.625],[0.4375,	1,	0.5625],[0.5,	1,	0.5],[0.5625,	1,	0.4375],[0.625,	1,	0.375],[0.6875,	1,	0.3125],[0.75,	1,	0.25],[0.8125,	1,	0.1875],[0.875,	1,	0.125],[0.9375,	1,	0.0625],[1,	1,	0],[1,	0.9375,	0],[1,	0.875,	0],[1,	0.8125,	0],[1,	0.75,	0],[1,	0.6875,	0],[1,	0.625,	0],[1,	0.5625,	0],[1,	0.5,	0],[1,	0.4375,	0],[1,	0.375,	0],[1,	0.3125,	0],[1,	0.25,	0],[1,	0.1875,	0],[1,	0.125,	0],[1,	0.0625,	0],[1,	0,	0],[0.9375,	0,	0],[0.875,	0,	0],[0.8125,	0,	0],[0.75,	0,	0],[0.6875,	0,	0],[0.625,	0,	0],[0.5625,	0,	0],[0.5,	0,	0]])
+        cmap=np.array([[0.041,0, 0],[0.083, 0, 0],[0.125,0,0],[0.166,0,0],[0.208,0,0],[0.250,0,0],[0.2917,0,0],[0.333,0,0],[0.3750,0,0],[0.4167,0,0],[0.4583,0,0],[0.5,0,0],[0.541,0,0],[0.583,0,0],[0.625,0,0],[0.666,0,0],[0.708,0,0],[0.750,0,0],[0.7917,0,0],[0.833,0,0],[0.875,0,0],[0.916,0,0],[0.958,0,0],[1,0,0],[1,0.041,0],[1,0.083,0],[1,0.125,0],[1,0.166,0],[1,0.208,0],[1,0.250,0],[1,0.291,0],[1,0.333,0],[1,0.375,0],[1,0.4167,0],[1,0.4583,0],[1,0.5,0],[1,0.541,0],[1,0.583,0],[1,0.625,0],[1,0.666,0],[1,0.708,0],[1,0.750,0],[1,0.7917,0],[1,0.833,0],[1,0.875,0],[1,0.916,0],[1,0.958,0],[1,1,0],[1,0.062,0],[1,1,0.125],[1,1,0.187],[1,1,0.250],[1,1,0.312],[1,1,0.375],[1,1,0.437],[1,1,0.5],[1,1,0.562],[1,1,0.625],[1,1,0.687],[1,1,0.75],[1,1,0.812],[1,1,0.875],[1,1,0.937],[1,1,1]]) # Mapa de color Hot
+        cmap=np.flipud(cmap) #invertir mapa de color
+        #cmap = np.array([[0.5,0.5,.5],[0,  0,  0.625],[0,  0,  0.6875],[0, 0,  0.75],[0,   0,  0.8125],[0, 0,  0.875],[0,  0,  0.9375],[0, 0,  1],[0,  0.0625, 1],[0,  0.125,  1],[0,  0.1875, 1],[0,  0.25,   1],[0,  0.3125, 1],[0,  0.375,  1],[0,  0.4375, 1],[0,  0.5,    1],[0,  0.5625, 1],[0,  0.625,  1],[0,  0.6875, 1],[0,  0.75,   1],[0,  0.8125, 1],[0,  0.875,  1],[0,  0.9375, 1],[0,  1,  1],[0.0625, 1,  0.9375],[0.125, 1,  0.875],[0.1875, 1,  0.8125],[0.25,  1,  0.75],[0.3125,  1,  0.6875],[0.375, 1,  0.625],[0.4375, 1,  0.5625],[0.5,   1,  0.5],[0.5625,   1,  0.4375],[0.625, 1,  0.375],[0.6875, 1,  0.3125],[0.75,  1,  0.25],[0.8125,  1,  0.1875],[0.875, 1,  0.125],[0.9375, 1,  0.0625],[1, 1,  0],[1,  0.9375, 0],[1,  0.875,  0],[1,  0.8125, 0],[1,  0.75,   0],[1,  0.6875, 0],[1,  0.625,  0],[1,  0.5625, 0],[1,  0.5,    0],[1,  0.4375, 0],[1,  0.375,  0],[1,  0.3125, 0],[1,  0.25,   0],[1,  0.1875, 0],[1,  0.125,  0],[1,  0.0625, 0],[1,  0,  0],[0.9375, 0,  0],[0.875,  0,  0],[0.8125, 0,  0],[0.75,   0,  0],[0.6875, 0,  0],[0.625,  0,  0],[0.5625, 0,  0],[0.5,    0,  0]])
         #  cmap = np.array([[0.5,.5,.5],[0,    0,  0.625],[0,  0,  0.6875],[0, 0,  0.75],[0,   0,  0.8125],[0, 0,  0.875],[0,  0,  0.9375],[0, 0,  1],[0,  0.0625, 1],[0,  0.125,  1],[0,  0.1875, 1],[0,  0.25,   1],[0,  0.3125, 1],[0,  0.375,  1],[0,  0.4375, 1],[0,  0.5,    1],[0,  0.5625, 1],[0,  0.625,  1],[0,  0.6875, 1],[0,  0.75,   1],[0,  0.8125, 1],[0,  0.875,  1],[0,  0.9375, 1],[0,  1,  1],[0.0625, 1,  0.9375],[0.125, 1,  0.875],[0.1875, 1,  0.8125],[0.25,  1,  0.75],[0.3125,  1,  0.6875],[0.375, 1,  0.625],[0.4375, 1,  0.5625],[0.5,   1,  0.5],[0.5625,   1,  0.4375],[0.625, 1,  0.375],[0.6875, 1,  0.3125],[0.75,  1,  0.25],[0.8125,  1,  0.1875],[0.875, 1,  0.125],[0.9375, 1,  0.0625],[1, 1,  0],[1,  0.9375, 0],[1,  0.875,  0],[1,  0.8125, 0],[1,  0.75,   0],[1,  0.6875, 0],[1,  0.625,  0],[1,  0.5625, 0],[1,  0.5,    0],[1,  0.4375, 0],[1,  0.375,  0],[1,  0.3125, 0],[1,  0.25,   0],[1,  0.1875, 0],[1,  0.125,  0],[1,  0.0625, 0],[1,  0,  0],[0.9375, 0,  0],[0.875,  0,  0],[0.8125, 0,  0],[0.75,   0,  0],[0.6875, 0,  0],[0.625,  0,  0],[0.5625, 0,  0],[0.5,    0,  0]])
+        print(np.shape(cmap))
         cmap = np.dot(255.0,cmap)
         Zrgb = Zrgb-np.amin(Zrgb)
         Zrgb = Zrgb / np.max(Zrgb)
@@ -202,8 +198,15 @@ class Main_L(object):
         idx = np.where(a == a.min())
         return idx
 
-l=Main_L()
-lead,verts,faces,channels,data,base =l.argparse()
+    def write2JSONFile (self,path, fileName, data):
+        filePathNameWExt = './'+path+'/'+fileName+'.json'
+        with open(filePathNameWExt, 'w') as fp:
+            json.dump(data,fp)
+
+# Cargar funciones y variables 
+l=Main_L() #Cargar la funciones necesarias para la ejecucion del programa
+path = './' #
+lead,verts,faces,channels,data,base =l.argparse() #Direccion y nombre de archivos (Lead_field,verts,faces,channels,EEG.txt,sujetobase.edf)
 Lead_field=lead # Ubicacion de la Lead Field
 verts=verts #Ubicacion de los verts
 faces=faces # Ubicacion de las faces
@@ -212,9 +215,10 @@ DataEEG=data # datos del EEG
 BaseEEG=base # etiquetas del EEG
 [L,verts,faces,channels]=l.load_BM_data(Lead_field,verts,faces,channels)#Loading Forward Model solution (LeadField)
 Y = np.loadtxt(DataEEG)#cargar datos del EEG
-Hdr,_= l.read_edf(BaseEEG)# Cargar parametros del EEG
+Hdr,record= l.read_edf(BaseEEG)# Cargar parametros del EEG
 print(Hdr["labels"])# Mostrar Etiquetas
 Y=Y.T
+
 # Source reconstruction
 TicToc = l.TicTocGenerator()# Comenzar contador
 l.tic() #Inicio del contador
@@ -224,15 +228,30 @@ print(np.shape(L))#Mostrar dimensiones de los datos de la Lead_Field
 J = l.source_reconstruction(Y,L,alfa,'sLOR')#Focalizar actividad ya sea con 'LOR':Loreta o 'sLOR': sLoreta
 J_energy=l.source_energy(J)# Energia de las fuentes
 J_energy=l.normalize_vector(J_energy) # normalizar los valores de energia 
+
 #Umbralizar los valores de la energia 
 for i in range(0,np.shape(J_energy)[0]):
-		if J_energy[i]<0.2:
-			J_energy[i] = 0.0
-
-print(np.shape(J_energy))#Mostrar dimensiones de las fuentes 
+        if J_energy[i]<0.2:
+            J_energy[i] = 0.0
+print(np.shape(J_energy))#Mostrar dimensiones de las fuentes
 l.toc()# Final del contador
 colors = l.IntensitytoJet(J_energy)
+colorExporter=colors[:,[0,1,2]]/255 # Seleccionar las tres primeras columnas y normalizar los valores del color 
+color={}
+color['items']=[]
+#Exportar matriz de color y energia
+for i in range(0,np.shape(colors)[0]):
+    rgbObj={}
+    rgbObj['r']=colorExporter[i,0] #organizar matriz a exportar por color rojo
+    rgbObj['g']=colorExporter[i,1] #organizar matriz a exportar por color verde
+    rgbObj['b']=colorExporter[i,2] #organizar matriz a exportar por color azul
+    color['items'].append(rgbObj)
+
+l.write2JSONFile(path,'colors',color) # exportar matriz de color con extension .json
+np.savetxt('J_energy.txt', J_energy, fmt='%.2e') # exportar matriz de J con extension .txt
 app = QtGui.QApplication([]) 
+
+#vizualizacion
 w = gl.GLViewWidget()
 w.show()
 w.setCameraPosition(distance=500)
